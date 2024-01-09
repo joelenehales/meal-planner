de4cc1dedf7d225671c2c74e790515119fc603ef
joelenehales <joelenehales@bell.net>
2024-01-08 20:38:03 -0500

    Begin adding remove button function + more docs

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let all_recipes = new Array(); // List of all recipes
-let added_recipes = new Array();  // List of selected recipes for the week
 let shopping_list = new Array();  // List of ingredients in selected recipes
+let currently_selected = -1; // ID of recipe currently in the selected viewer
 
-// TODO: Class works, need to add documentation
+/**
+ * TODO: Documentation
+ */
 class Ingredient {
 
     constructor(name) {
@@ -12,7 +14,9 @@ class Ingredient {
 }
 
 
-// TODO: Class works, need to add documentation
+/**
+ * TODO: Documentation
+ */
 class Recipe {
 
     constructor(name, ingredients) {
@@ -59,44 +63,64 @@ const tomato_cherry = new Ingredient('Cherry Tomato');
 /* Define recipes */
 const bagel_sandwich = new Recipe('Bagel Sandwich', [arugula, bagel, bacon, egg]);
 const shakshouka = new Recipe('Shakshouka', [tomato_canned, onion, egg]);
+const roasted_potatoes = new Recipe('Roasted Potatoes', [potato]);
 
 
 
-// TODO: Function works, need to add documentation
+/**
+ * Displays the list of recipes.
+ * TODO: Make function display the recipes in alphabetical order
+ */
 function displayRecipes() {
-    
-    let recipe_list_HTML = document.getElementById("recipe-list");
 
-    for (i = 0; i < all_recipes.length; ++i) {
+    var recipe_list_HTML = document.getElementById("recipe-list");  // Reference to the HTML element displaying the list of recipes
+
+    for (i = 0; i < all_recipes.length; ++i) {  // Iterate over all recipes
 
         let recipe = all_recipes[i];
-
+        
+        // Create HTML element for each recipe
         let li = document.createElement('li');
         li.innerHTML = `<button id="${recipe.id_num}" onclick="select(${recipe.id_num})">${recipe.name}</button>`;
-        recipe_list_HTML.appendChild(li);
+        recipe_list_HTML.appendChild(li);  // Add recipe to the existing list
     }
 
 }
 
 
-// Function moves a recipe to the "Selected" viewer when clicked
+/**
+ * Updates the Add To Menu button to add the recipe with the given ID to the
+ * menu when clicked.
+ * @param {number} recipe_id Recipe ID of the recipe to add to the menu. Equal
+ * to its index in the recipe list array.
+ */
+function allowAdd(recipe_id) {
+
+    var add_button = document.getElementById("add-button");
+    add_button.disabled = false;  // Allow button to be clicked
+    add_button.addEventListener('click', function(){addToMenu(recipe_id)}, {once: true});  // Tie click event to the function to add the recipe with the given ID
+
+}
+
+
+/**
+ * Moves the recipe with the given ID number to the "Selected" viewer
+ * @param {number} recipe_id Recipe ID of the recipe to view. Equal to its index
+ * in the recipe list array.
+ */
 function select(recipe_id) {
 
-    let selected_recipe_div_HTML = document.getElementById("selected-recipe");
+    let selected_recipe_div_HTML = document.getElementById("selected-recipe");  // Reference to the HTML element displaying the selected recipe to view
+
+    currently_selected = recipe_id;
+
     let recipe = all_recipes[recipe_id];
 
-    selected_recipe_div_HTML.innerHTML = `<p>${recipe.name}</p>`; // TODO: fix this
+    selected_recipe_div_HTML.innerHTML = `<p>${recipe.name}</p>`;
 
-    var add_remove_button = document.createElement('button');
-
-    if (!recipe.is_added) {
-
-        add_remove_button.innerText = "Add to menu";
-        add_remove_button.addEventListener('click', function(){addToMenu(recipe_id)});
-
+    if (recipe.is_added === false) {
+        allowAdd(recipe_id);
     }
-    
-    selected_recipe_div_HTML.appendChild(add_remove_button);
 
 }
 
@@ -108,9 +132,31 @@ function addToMenu(recipe_id) {
     let recipe = all_recipes[recipe_id];
 
     // Create new list item for the recipe added
-    let li = document.createElement('li');  
-    li.innerHTML = `<p>${recipe.name}</p>`;
+    let li = document.createElement('li');
+    li.setAttribute("id", `menu-list-${recipe_id}`);
+
+    li.innerHTML = `<p>${recipe.name}</p><button onclick="removeFromMenu(${recipe.id_num})">Remove</button>`;
+    
     menu_div_HTML.appendChild(li);
+
+    recipe.is_added = true;
+
+    // Update the add to menu button to be disabled again
+    let add_button = document.getElementById("add-button");
+    add_button.disabled = true;
+
+}
+
+function removeFromMenu(recipe_id) {
+
+    let recipe_HTML = document.getElementById(`menu-list-${recipe_id}`);
+    recipe_HTML.remove();
+
+    let recipe = all_recipes[recipe_id];
+    recipe.is_added = false;
+    
+    select(currently_selected);  // Resets the "Add To Menu" button
+
 
 }
 
